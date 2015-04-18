833eefaaa5c49787700594aecfd5a8e6ddc15701
magdalena <magdalenawlodek87@gmail.com>
2015-04-18 13:21:50 +0200

    fix identations in controllers and add skip before action in home controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,15 +1,15 @@
 class CountriesController < ApplicationController
-	
-  before_action :fetch_country, only:[:show, :edit, :update, :destroy]
-	
 
-	
+  before_action :fetch_country, only:[:show, :edit, :update, :destroy]
+
+
+
   def index
 	  @countries = Country.all
   end
 
   def show
-	
+
   end
 
   def new
@@ -17,40 +17,38 @@ class CountriesController < ApplicationController
   end
 
   def create
-  	  @country = Country.new(country_params)
- 
-  	  if @country.save
-   	 	redirect_to @country
-  	  else
-    	render 'new'
-  	  end
+	  @country = Country.new(country_params)
+
+	  if @country.save
+ 	 	redirect_to @country
+	  else
+  	render 'new'
+	  end
   end
 
   def destroy
-
-      @country.destroy
-      redirect_to countries_path
+    @country.destroy
+    redirect_to countries_path
   end
- 
+
   def edit
 
   end
 
   def update
- 
-  	  if @country.update(country_params)
-    	redirect_to @country
-  	  else
-    	render 'edit'
-  	  end
+	  if @country.update(country_params)
+  	redirect_to @country
+	  else
+  	render 'edit'
+	  end
   end
- 
+
   private
-  	  def country_params
-    	params.require(:country).permit(:name)
+	  def country_params
+  	params.require(:country).permit(:name)
  	  end
 
  	  def fetch_country
-        @country = Country.find(params[:id]) 
-      end
+      @country = Country.find(params[:id])
+    end
   end
--- app/controllers/travel_modes_controller.rb
+++ app/controllers/travel_modes_controller.rb
@@ -1,7 +1,7 @@
 class TravelModesController < ApplicationController
-	
+
 	before_action :fetch_travel_mode, only:[:show, :edit, :update, :destroy]
-	
+
 	def index
 		@travel_modes = TravelMode.all
 	end
@@ -12,43 +12,43 @@ class TravelModesController < ApplicationController
 
 	def create
 		@travel_mode = TravelMode.new(travel_mode_params)
- 
-  		if @travel_mode.save
-   	 		redirect_to @travel_mode
-  		else
-    		render 'new'
-    	end
+
+		if @travel_mode.save
+ 	 		redirect_to @travel_mode
+		else
+  		render 'new'
+  	end
 	end
 
 	def show
-		
+
 	end
 
 	def edit
-		
+
 	end
 
 	def update
-		
+
 		if @travel_mode.update(travel_mode_params)
-    		redirect_to @travel_mode
-  		else
-    		render 'edit'
-    	end
+    	redirect_to @travel_mode
+  	else
+    	render 'edit'
+    end
 	end
 
 	def destroy
-		
 		@travel_mode.destroy
- 
-    	redirect_to travel_modes_path
+ 		redirect_to travel_modes_path
 	end
 
-	private 
+	private
+
 	def travel_mode_params
 		params.require(:travel_mode).permit(:name)
 	end
+
 	def fetch_travel_mode
-        @travel_mode = TravelMode.find(params[:id]) 
-    end
+    @travel_mode = TravelMode.find(params[:id])
+  end
 end
