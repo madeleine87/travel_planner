c3edd80cbc56f9b477d109854eaf4371ce8de8cc
magdalena <magdalenawlodek87@gmail.com>
2015-04-18 18:20:59 +0200

    add cancan permissions to ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,21 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-      user ||= User.new # guest user (not logged in)
-      if user.role?(:admin)
-        can :manage, :all
-      else
-        can :read, :all
+    user ||= User.new # guest user (not logged in)
+    if user.role?(:admin)
+      can :manage, :all
+    else if user.role?(:user)
+      can :manage, User do |u|
+        u.id == user.id
       end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+      # can :manage Journey do |j|
+      #   j.user_id == user.id
+      # end
+    end
+    else
+      can :read, :all
+    end
+
+
   end
 end
